2eec6187bc32f09dd0fe5b95490b2960fa230e61
Habeblahi <Habeebajuwon@gmail.com>
2023-03-13 23:16:41 -0700

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - concatenates all the arguments of your program
+ *@str: string
+ * Return: a pointer to a new string
+ */
+char **strtow(char *str)
+{
+	int i, w, j, k, count, m, wordf;
+	char **p;
+	char *x;
+
+	w = 0;
+	j = 0;
+	i = 0;
+	count = 0;
+	if (*str == '\0' || str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
+			w++;
+	}
+	p = (char **)malloc((w + 1) * sizeof(char *));
+	if (p == NULL)
+		return (NULL);
+	for (wordf = 0; str[wordf] && j <= w; wordf++)
+	{
+		count = 0;
+		if (str[wordf] != ' ')
+		{
+			for (i = wordf ; str[i] != '\0'; i++)
+			{
+				if (str[i] == ' ')
+					break;
+				count++;
+}
