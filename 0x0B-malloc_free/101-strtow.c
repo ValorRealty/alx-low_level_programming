13f0fb44dbb09726ccbce10ec209438cef954ad2
Habeblahi <Habeebajuwon@gmail.com>
2023-03-14 01:09:37 -0700

    updated

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -46,28 +46,3 @@ while (*(str + len) != ' ' && *(str + len))
 {
 len += 1;
 }
-len += 1;
-col = malloc(sizeof(char) * len);
-if (!col)
-{
-for (k = j - 1; k >= 0; k--)
-{
-free(f[k]);
-}
-free(f);
-return (NULL);
-}
-for (k = 0; k < (len - 1);  k++)
-{
-*(col + k) = *(str++);
-}
-*(col + k) = '\0';
-*(f + j) = col;
-if (j < (count - 1))
-{
-j++;
-}
-}
-*(f + j) = NULL;
-return (f);
-}
