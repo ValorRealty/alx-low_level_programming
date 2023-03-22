d023906ab9a0621998ac33dcb4beca2617b5fb58
Habeblahi <Habeebajuwon@gmail.com>
2023-03-22 07:26:46 -0700

    updated

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,75 +1,29 @@
-#include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+#include "3-calc.h"
 /**
-  * op_add - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
-int op_add(int a, int b)
+ * get_op_func - matches operator from main
+ * @s: op str
+ * Return: 0
+ */
+int (*get_op_func(char *s))(int, int)
 {
-	return (a + b);
-}
+	op_t op_s[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 
-/**
-  * op_sub - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
+	int i = 0;
 
-/**
-  * op_mul - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-
-/**
-  * op_div - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
-int op_div(int a, int b)
-{
-	if (b == 0)
+	while (op_s[i].op)
 	{
-		printf("Error\n");
-		exit(100);
+		if (*(op_s[i].op) == *s)
+			return (op_s[i].f);
+		i++;
 	}
-
-	return (a / b);
-}
-
-/**
-  * op_mod - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
-int op_mod(int a, int b)
-{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	return (a % b);
+	return (NULL);
 }
